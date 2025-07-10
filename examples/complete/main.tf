916815d0e3575914e5a74258bea4dcf401af96c2
spartan-tainguyen <tai.nguyen@c0x12c.com>
2025-07-10 10:03:30 +0700

    update example

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -6,6 +6,6 @@ module "provider" {
   client_id_list = ["sts.amazonaws.com"]
   additional_thumbprints = [
     "6938fd4d98bab03faadb97b34396831e3780aea1",
-    "1c58a3a8518e8759bf075b76b750d4f2df264fcd"
+    "1c58a3a8518e8759bf075b76b750d4f2df264fcd",
   ]
 }
